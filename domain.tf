1b09f72696469214e6766062944f53d0cb6db268
Brandon Gottlob <info@bgottlob.com>
2023-02-11 13:15:41 -0500

    Host blog using registry

--- domain.tf
+++ domain.tf
@@ -10,7 +10,7 @@ resource "linode_domain_record" "bgottlob_blog" {
   depends_on = [linode_lke_cluster.personal]
   domain_id = linode_domain.bgottlob.id
   record_type = "A"
-  name = "blog"
+  name = "bgottlob.com"
   target = local.ingress_external_ip
 }
 
