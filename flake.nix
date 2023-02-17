7e041393e4895e10b43632a9ad4eb7939c731422
Brandon Gottlob <info@bgottlob.com>
2023-02-16 23:07:42 -0500

    Add Nix flake and direnv config

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "nixpkgs/22.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.kubectl
+          pkgs.terraform
+        ];
+      };
+    });
+}
