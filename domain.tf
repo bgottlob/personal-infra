312af02636a8260905fe8895470599f4238962b8
Brandon Gottlob <info@bgottlob.com>
2023-02-23 22:17:14 -0500

    Fix name attribute for bgottlob.com DNS record
    This is not a functional change, but will prevent the Linode Terraform
    provider from showing changes when running plans.

--- domain.tf
+++ domain.tf
@@ -10,7 +10,6 @@ resource "linode_domain_record" "bgottlob_blog" {
   depends_on = [linode_lke_cluster.personal]
   domain_id = linode_domain.bgottlob.id
   record_type = "A"
-  name = "bgottlob.com"
   target = local.ingress_external_ip
 }
 
