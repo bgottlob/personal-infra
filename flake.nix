8420a64efb7ab4af6e2d0cf08671f7dcec18f834
Brandon Gottlob <info@bgottlob.com>
2023-03-28 20:06:44 -0400

    Use with in Nix flake

--- flake.nix
+++ flake.nix
@@ -6,12 +6,14 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = [
-          pkgs.kubectl
-          pkgs.terraform
-        ];
-      };
-    });
+    in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          packages = [
+            kubectl
+            terraform
+          ];
+        };
+      });
 }
