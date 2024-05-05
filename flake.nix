5e77c2c9452a58877c7c95401458ecb0461a9365
Brandon Gottlob <info@bgottlob.com>
2024-05-04 21:32:10 -0400

    Add packages to flake, upgrade nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "nixpkgs/22.11";
+  inputs.nixpkgs.url = "nixpkgs/23.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -11,7 +11,14 @@
       {
         devShells.default = mkShell {
           packages = [
+            jsonnet
+            jsonnet-bundler
             kubectl
+            kubernetes-helm
+            openssl
+            postgresql
+            sops
+            tanka
             terraform
           ];
         };
