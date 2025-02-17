c4823abcff36cbbaae329ac8e2a7aafb214ef470
Brandon Gottlob <info@bgottlob.com>
2025-02-17 13:04:08 -0500

    Upgrade to k8s 1.31

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ provider "linode" {
 
 resource "linode_lke_cluster" "personal" {
   label = "bgottlob-personal"
-  k8s_version = "1.30"
+  k8s_version = "1.31"
   region = "us-east"
 
   control_plane {
