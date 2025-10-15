a8085f4831d63f277a6c11baf1bce0d26f23eb41
Brandon Gottlob <info@bgottlob.com>
2025-10-14 23:29:25 -0400

    Add yoke and WASM target to flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,43 @@
 {
   description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "nixpkgs";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      yokeVersion = "0.15.6";
+      overlays = [
+        (import rust-overlay)
+        (final: prev: {
+           yoke = pkgs.buildGoModule rec {
+             pname = "yoke";
+             version = yokeVersion;
+             src = pkgs.fetchFromGitHub {
+               owner = "yokecd";
+               repo = "yoke";
+               tag = "yokecd/${yokeVersion}";
+               hash = "sha256-W8ouqrIB4RHStZ2Yhn+wzq4emMkhjA8Vi9Cc0p46Emo=";
+             };
+	     vendorHash = "sha256-/4IINHW+T8vGBP166rdSLickki1Wh30/77K49hlwLM4=";
+	     # Tests for this build a kind cluster, so skip that
+	     doCheck = false;
+           };
+         })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      rust = pkgs.rust-bin.stable.latest.default.override {
+        targets = [ "wasm32-wasip1" ];
+      };
     in
       with pkgs;
       {
         devShells.default = mkShell {
           packages = [
+            cargo-wasi
             jsonnet
             jsonnet-bundler
             kubectl
@@ -21,7 +48,10 @@
             sops
             tanka
             velero
+            yoke
+            pkg-config
           ];
+          nativeBuildInputs = [rust];
         };
       });
 }
