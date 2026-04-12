d730bf47b9e0f42d0736beaeb88035a79e07bbc7
Brandon Gottlob <info@bgottlob.com>
2026-04-12 19:06:02 -0400

    Update yoke templates for Helm library changes

--- templates/yoke-helm-deployment/build.rs
+++ templates/yoke-helm-deployment/build.rs
@@ -10,32 +10,11 @@ const NAMESPACE: &str = "{{ namespace }}";
 const REPO_NAME: &str = "{{ chart_repo_name }}";
 const REPO_URL: &str = "{{ chart_repo_url }}";
 
-fn values() -> serde_json::Value {
-    json!({
-        // Fill these in with the values to be passed to the Helm chart
-        "example-number": 1234,
-        "example-obj": {
-            "example-str": "abcd",
-        },
-    })
-}
-
 fn main() -> anyhow::Result<()> {
-    if !helm::repo_exists(REPO_NAME, REPO_URL) {
-        helm::add_repo(REPO_NAME, REPO_URL)
-    }
+    helm::ensure_repo(REPO_NAME, REPO_URL)?;
+
     let out_dir = env::var("OUT_DIR")?;
     let out_path = Path::new(&out_dir);
-
-    let secrets_path = std::env::var("SECRETS_FILE").unwrap_or("../secrets.yaml".into());
-    let secrets = secrets::decrypt_parse_secrets(&secrets_path)
-        .unwrap_or_else(|_| panic!("Should parse {} file into Secrets struct", secrets_path));
-
-    let example_secret = "example";
-    println!(
-        "cargo:rustc-env=EXAMPLE_SECRET={}",
-        example_secret,
-    );
 
     helm::pull(Some(REPO_NAME), CHART_NAME, CHART_VERSION, out_path)?;
 
@@ -43,32 +22,20 @@ fn main() -> anyhow::Result<()> {
         File::create(out_path.join("helm-output.yaml"))?
     );
 
-    let values_path = out_path.join("values.yaml");
-    {
-        let mut values_file = BufWriter::new(
-            File::create(values_path.clone())?
-        );
-
-        let values = values();
-        let values_str = serde_norway::to_string(&values).expect("values should serialize to yaml");
-        write!(&mut values_file, "{}", values_str)?;
-    }
-    // using the scope above closes the values file so that it is fully written
-    // to before helm template needs to use it
-
-    let template = helm::template(
-        CHART_NAME,
-        CHART_VERSION,
-        NAMESPACE,
-        Some("{{ project-name }}"),
-        None,
-        Some(Path::new("values.yaml")),
-        out_path
-    )?;
+    let template = helm::template(CHART_NAME, CHART_VERSION, NAMESPACE, helm::TemplateOptions {
+        release_name: "{{ project-name }}",
+        values: Some(json!({
+            // Fill these in with the values to be passed to the Helm chart
+            "example-number": 1234,
+            "example-obj": {
+                "example-str": "abcd",
+            },
+        })),
+        ..Default::default()
+    }, out_path)?;
     write!(&mut out_file, "{}", template)?;
 
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed={}", secrets_path);
 
     Ok(())
 }
